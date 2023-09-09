9d2355ac68b62f711fb5289a643ab7bba48a975b
Meritava <georgemeritava@gmail.com>
2023-09-09 04:22:57 +0100

    m

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,10 +1,11 @@
-// Define an object with a property containing the old value
-let myObject = {
-    myProperty: 12
-};
-
-// Replace the old value (12) with the new value (89)
-myObject.myProperty = 89;
-
-// Now the object contains the updated value
-console.log(myObject.myProperty); // Output: 89
+#!/usr/bin/node
+const myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+  
+  // Replace the value property with 89
+  myObject.value = 89;
+  
+  console.log(myObject);
