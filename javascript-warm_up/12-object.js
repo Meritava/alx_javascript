9a8b91cf5eb8ae1c1bb429eeeffa19b1ce6494b9
Meritava <georgemeritava@gmail.com>
2023-09-09 03:37:59 +0100

    m

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,7 +1,8 @@
-const numbers = [3, 5, 12, 7, 1];
+let numberr = 12;
 
-// Update the value 12 with 89
-numbers[2] = 89;
+function change_value(){
+    numberr = 89;
+}
 
-// Print the updated array
-console.log(numbers);
+change_value();
+console.log(numberr);
