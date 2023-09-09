1cb7774c2e502529bea087111c0d33ac62149492
Meritava <georgemeritava@gmail.com>
2023-09-09 04:29:24 +0100

    m

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,3 +1,12 @@
-function add (a, b) {
-    return (a + b);
-}
+// 13-add.js
+
+// Define the add function
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Export the add function so it's visible outside this module
+  module.exports = {
+    add: add
+  };
+  
