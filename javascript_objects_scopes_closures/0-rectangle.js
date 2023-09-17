6e511e9acc2624295eadcd1ec8405cbed9b9c8ce
Meritava <georgemeritava@gmail.com>
2023-09-17 13:21:44 +0100

    m

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,5 +1,10 @@
 class Rectangle {
+    /* 
+    An empty class Rectangle that defines rectangle
+    */
     constructor() {
-        
+        /*
+        a constructor that initializes the code
+        */
     }
 }
