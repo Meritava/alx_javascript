9e20d9bd06c1fb49ade9a409f08db0478beeeb19
Meritava <georgemeritava@gmail.com>
2023-09-22 02:40:43 +0100

    m

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -3,10 +3,10 @@
 // import request
 const request = require('request');
 // pass cmd line argument
-const url = process.argv[2]
+const url = process.argv[2];
 // make a GET request
 request.get(url)
- .on('response', response => {
-    console.log('code: ', response.statusCode);
- })
+       .on('response', response => {
+           console.log('code: ', response.statusCode);
+       })
 
