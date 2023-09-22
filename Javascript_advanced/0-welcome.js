8522917802e49de22d39e39c62c17f1021b5cb8e
Meritava <georgemeritava@gmail.com>
2023-09-22 09:52:29 +0100

    m

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function welcome(firstName, lastName) {
+    const fullName = firstName + " " + lastName;
+    function displayFullName() {
+        alert("Welcome " + fullName + "!");
+    }
+    displayFullName()
+};
